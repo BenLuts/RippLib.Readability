46b017d57cbe1fb1f099ad5754c978850117f88a
BenLuts <lutsben@gmail.com>
2018-10-05 11:08:54 +0200

    Added Expando Extensions
    Added Expando extensions for easy access

---
+++ RippLib.Util/Extensions/Expando/ExpandoObjectExt.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+
+namespace RippLib.Util.Extensions
+{
+    public static class ExpandoObjectExt
+    {
+        public static void AddProperty(this ExpandoObject expando, string propertyName, object propertyValue)
+        {
+            // ExpandoObject supports IDictionary so we can extend it like this
+            var expandoDict = expando as IDictionary<string, object>;
+            if (expandoDict.ContainsKey(propertyName))
+                expandoDict[propertyName] = propertyValue;
+            else
+                expandoDict.Add(propertyName, propertyValue);
+        }
+    }
+}
