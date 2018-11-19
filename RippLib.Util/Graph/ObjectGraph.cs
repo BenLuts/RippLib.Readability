ea376a8227714c613bffe42c3e40c23d4591169d
BenLuts <lutsben@gmail.com>
2018-11-19 13:46:48 +0100

    ObjectGrapg
    Added ObjectGraph. I use this often, so put it in lib.

---
+++ RippLib.Util/Graph/ObjectGraph.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RippLib.Util.Graph
+{
+    public class ObjectGraph
+    {
+        public Dictionary<Type, string[]> GraphingDic { get; set; }
+
+        public ObjectGraph()
+        {
+            GraphingDic = new Dictionary<Type, string[]>();
+        }
+
+        public bool Contains(Type type, string propertyName)
+        {
+            return GraphingDic.ContainsKey(type) && GraphingDic[type].Contains(propertyName);
+        }
+        public void AddGraph(Type type, params string[] properties)
+        {
+            if (null != properties)
+            {
+                if (GraphingDic.ContainsKey(type))
+                {
+                    var startPos = GraphingDic[type].Length;
+                    var newArray = new string[GraphingDic[type].Length + properties.Length];
+                    GraphingDic[type].CopyTo(newArray, 0);
+                    for (var i = 0; i < properties.Length; i++)
+                    {
+                        newArray[i + startPos] = properties[i];
+                    }
+                    GraphingDic[type] = newArray;
+                }
+                else
+                {
+                    GraphingDic.Add(type, properties);
+                }
+            }
+        }
+    }
+}
