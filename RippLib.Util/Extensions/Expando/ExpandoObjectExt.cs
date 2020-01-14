2ae7ca49539ba185b7413b6e6650ed57ee7c52e8
BenLuts <37296304+BenLuts@users.noreply.github.com>
2020-01-14 21:06:29 +0100

    Feature/code clean (#2)
    * Updated Single Property Dictionary to use Linq & made obsolete
    
    * Bumped up version
    
    * Cleaned usings
    
    * Moved CollectionUtil to more logical folder
    Updated travis yml file with safelist

--- RippLib.Util/Extensions/Expando/ExpandoObjectExt.cs
+++ RippLib.Util/Extensions/Expando/ExpandoObjectExt.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Dynamic;
-using System.Linq;
-using System.Text;
 
 namespace RippLib.Util.Extensions
 {
