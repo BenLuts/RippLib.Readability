4f14b39a574a367b4120eeafe43f47e520369d7b
Ben Luts <ben.luts@allphi.eu>
2024-07-24 14:15:24 +0200

    Updated namespaces for extentions on collections! Improved Test cases. Removed Obsolete methods. Added Test support for multiple targets

--- RippLib.Util.Tests/CollectionTest.cs
+++ RippLib.Util.Tests/CollectionTest.cs
@@ -1,166 +1,27 @@
+using RippLib.Readability;
 using RippLib.Util.Tests.POCO;
 using System;
-using System.Collections.Generic;
 using System.Linq;
 using Xunit;
 
-namespace RippLib.Util.Tests
+namespace RippLib.Util.Tests;
+
+public class CollectionTests
 {
-    public class CollectionTest
+    [Fact]
+    public void TestShuffle()
     {
-        [Fact]
-        public void TestDictionaryExtension()
-        {
-            var list = SimplePoco.CreateDummyList(200);
-            var dic = list.CreateDictionaryFromPropertyValues(x => x.CharSequence);
-            Assert.NotNull(dic);
-        }
+        var list = SimplePoco.CreateDummyList(20);
+        var shuffledList = list.Shuffle().ToList();
+        Assert.True(list.Count == shuffledList.Count && list[0].ID != shuffledList[0].ID);
+    }
 
-        [Fact]
-        public void TestShuffle()
-        {
-            var list = SimplePoco.CreateDummyList(20);
-            var shuffledList = list.Shuffle().ToList();
-            Assert.True(list.Count == shuffledList.Count && list[0].ID != shuffledList[0].ID);
-        }
-
-        [Fact]
-        public void TestBatch()
-        {
-            var list = SimplePoco.CreateDummyList(20);
-            var batchList = list.Batch(2).ToList();
-            Assert.True(batchList.Count == 10);
-            batchList.ForEach(x => Assert.True(x.Count() == 2));
-        }
-
-        [Fact]
-        public void Empty_Null_ReturnsTrue()
-        {
-            //Arrange
-            var list = (List<object>)null;
-
-            //Act
-            var result = list.Empty();
-
-            //Assert
-            Assert.True(result);
-        }
-
-        [Fact]
-        public void Empty_ReturnsTrue()
-        {
-            //Arrange
-            var list = new List<object>();
-
-            //Act
-            var result = list.Empty();
-
-            //Assert
-            Assert.True(result);
-        }
-
-        [Fact]
-        public void Empty_WithItems_ReturnsTrue()
-        {
-            //Arrange
-            var list = new List<object>() { new() };
-
-            //Act
-            var result = list.Empty();
-
-            //Assert
-            Assert.False(result);
-        }
-
-        [Fact]
-        public void Empty_WithItems_LambdaExpression_ReturnsTrue()
-        {
-            //Arrange
-            var list = new List<string>() { "not so empty string", "another not so empty string" };
-
-            //Act
-            var result = list.Empty(x => x.StartsWith("values"));
-
-            //Assert
-            Assert.True(result);
-        }
-
-        [Fact]
-        public void Empty_WithItems_LambdaExpression_Matches_ReturnsFalse()
-        {
-            //Arrange
-            var list = new List<string>() { "not so empty string", "another not so empty string" };
-
-            //Act
-            var result = list.Empty(x => x.StartsWith("not"));
-
-            //Assert
-            Assert.False(result);
-        }
-
-        [Fact]
-        public void NotEmpty_Null_ReturnsFalse()
-        {
-            //Arrange
-            var list = (List<object>)null;
-
-            //Act
-            var result = list.NotEmpty();
-
-            //Assert
-            Assert.False(result);
-        }
-
-        [Fact]
-        public void NotEmpty_ReturnsFalse()
-        {
-            //Arrange
-            var list = new List<object>();
-
-            //Act
-            var result = list.NotEmpty();
-
-            //Assert
-            Assert.False(result);
-        }
-
-        [Fact]
-        public void NotEmpty_WithItems_ReturnsTrue()
-        {
-            //Arrange
-            var list = new List<object>() { new() };
-
-            //Act
-            var result = list.NotEmpty();
-
-            //Assert
-            Assert.True(result);
-        }
-
-        [Fact]
-        public void NotEmpty_WithItems_LambdaExpression_ReturnsFalse()
-        {
-            //Arrange
-            var list = new List<string>() { "not so empty string", "another not so empty string" };
-
-            //Act
-            var result = list.NotEmpty(x => x.StartsWith("values"));
-
-            //Assert
-            Assert.False(result);
-        }
-
-        [Fact]
-        public void NotEmpty_WithItems_LambdaExpression_Matches_ReturnsTrue()
-        {
-            //Arrange
-            var list = new List<string>() { "not so empty string", "another not so empty string" };
-
-            //Act
-            var result = list.NotEmpty(x => x.StartsWith("not"));
-
-            //Assert
-            Assert.True(result);
-        }
+    [Fact]
+    public void TestBatch()
+    {
+        var list = SimplePoco.CreateDummyList(20);
+        var batchList = list.Batch(2).ToList();
+        Assert.Equal(10, batchList.Count);
+        batchList.ForEach(x => Assert.Equal(2, x.Count()));
     }
 }
--- RippLib.Util.Tests/POCO/SimplePoco.cs
+++ RippLib.Util.Tests/POCO/SimplePoco.cs
@@ -1,29 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 
-namespace RippLib.Util.Tests.POCO
+namespace RippLib.Util.Tests.POCO;
+
+public class SimplePoco
 {
-    public class SimplePoco
-    {
-        public int ID { get; set; }
-        public string CharSequence { get; set; }
+    public int ID { get; set; }
+    public string CharSequence { get; set; }
 
-        public static List<SimplePoco> CreateDummyList(int numberOfSequences)
+    public static List<SimplePoco> CreateDummyList(int numberOfSequences)
+    {
+        var random = new Random();
+        var list = new List<SimplePoco>();
+        for (int i = 0; i < numberOfSequences; i++)
         {
-            var random = new Random();
-            var list = new List<SimplePoco>();
-            for (int i = 0; i < numberOfSequences; i++)
+            list.Add(new SimplePoco
             {
-                list.Add(new SimplePoco
-                {
-                    ID = i,
-                    CharSequence = new string(Enumerable.Repeat("ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789", 5)
-                        .Select(s => s[random.Next(s.Length)]).ToArray())
-                });
-            }
-            return list;
+                ID = i,
+                CharSequence = new string(Enumerable.Repeat("ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789", 5)
+                    .Select(s => s[random.Next(s.Length)]).ToArray())
+            });
         }
+        return list;
     }
 }
